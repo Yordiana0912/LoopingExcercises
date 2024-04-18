b30a796ece851095fe5b35aea00dae2b97ed60e1
Yordiana0912 <Yordiana0912@gmail.com>
2024-04-18 11:44:38 -0400

    Completed

---
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) throws InterruptedException{
+        for (int i = 0; i < 10; i++) {
+            System.out.println("thread.sleep(1000)");
+        }
+
+
+    }
+}
---
+++ src/main/java/com/pluralsight/Whileloop.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import java.sql.SQLOutput;
+
+public class Whileloop {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println( " I love java");
+            i++;
+
+    }
+
+
+}}
